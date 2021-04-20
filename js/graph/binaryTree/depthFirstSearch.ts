1303172b7b9283542b86805b3ae6b74ed128a36e
Ryan <ryan.zheng.1998@gmail.com>
2021-04-21 07:03:07 +0800

    2021 04 21

---
+++ js/graph/binaryTree/depthFirstSearch.ts
@@ -0,0 +1,12 @@
+interface TreeNode {
+    val: number;
+    right: TreeNode | null;
+    left: TreeNode | null;
+}
+
+const depthFirstSearch = (root: TreeNode | null) => {
+    const leftVal = depthFirstSearch(root.left)
+    const rightVal = depthFirstSearch(root.right)
+
+    return [...leftVal, root.val, ...rightVal]
+}
